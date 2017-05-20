d041396cfc6cbca866433d5a74237f0f7e44d722
wharsojo <wharsojo@gmail.com>
2017-05-20 16:17:11 +0800

    update eQuakeApp

--- ex/eQuakeApp.js
+++ ex/eQuakeApp.js
@@ -2,6 +2,7 @@
 // https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson
 // https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
 const eQuakeApp = () => {
+    const _ = {world: null, equake: null};
     return {
         name: 'eQuakeApp',
         urls: [
@@ -9,11 +10,10 @@ const eQuakeApp = () => {
             'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson',
         ],
         onReady(err, world, equake) {
-            const features = equake.features.filter(d => d.properties.mag>3);
+            _.world = world;
+            _.equake = equake;
+            this.eQuakeApp.mag(3);
             this.worldCanvas.data({world});
-            this.barPlugin.data({features});
-            this.dotsCanvas.data({features});
-            this.pingsCanvas.data({features});
         },
         onInit() {
             this.register(earthjs.plugins.commonPlugins());
@@ -22,7 +22,14 @@ const eQuakeApp = () => {
             this.register(earthjs.plugins.barPlugin());
             this.commonPlugins.addChecker('showPings:Pings:showPings'.split(':'));
             this.commonPlugins.addChecker('showDots:EQuake:showDots'.split(':'));
-        }
+        },
+        mag(mag) {
+            const features = _.equake.features.filter(d => d.properties.mag>=mag);
+            this.barPlugin.data({features});
+            this.dotsCanvas.data({features});
+            this.pingsCanvas.data({features});
+            this.svgDraw();
+        },
     }
 }
 earthjs.plugins.eQuakeApp = eQuakeApp;
