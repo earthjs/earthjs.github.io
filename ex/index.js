304559e6fa79c91ee664337c5a1c20c7255a3259
wharsojo <wharsojo@gmail.com>
2017-09-14 07:34:45 +0800

    update to the latest earthjs

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,4 @@
-var g = earthjs({width: 1200, height: 900, transparent: true});
+var g = earthjs({width: 1200, height: 950, transparent: true, padding: 80});
 g.register(earthjs.plugins.eQuakeApp());
 g.ready(function() {
     g.create();
